5bbb424fe92b1810788f65b499801504eaf54062
silvio Molina <molinahuel44@gmail.com>
2020-12-15 11:20:44 -0300

    first commit from android, using termux, trying to understsnd the command pattern

---
+++ main.py
@@ -0,0 +1,10 @@
+#invoker
+class ICommand():
+  
+class switch_on_command():
+  
+class swigch_off_command():
+
+if __init__:
+  SWITCH_ON = switch_on_command()
+  SWITCH_OFF = switch_off_command():
